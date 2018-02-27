bcbe9312dd9db0e289948e8cbcbd1f011871b4b2
Johaneskrauzer <36884352+Johaneskrauzer@users.noreply.github.com>
2018-02-27 14:28:59 +0100

    Add files via upload

---
+++ create 1010 fichier.ps1
@@ -0,0 +1,6 @@
+﻿#créer dossier
+
+mkdir C:\Users\caca\Desktop\Pl1DeFichier
+#Creer 100 fichier txt
+
+1..10000 | foreach {New-Item C:\Users\caca\Desktop\Pl1DeFichier\Ceciestlaproceduren.$_.txt -ItemType file}
